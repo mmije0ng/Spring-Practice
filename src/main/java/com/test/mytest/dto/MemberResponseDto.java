85c31f11c5774affdf78810bd9c75875d2f173c1
mijeong <victon777@naver.com>
2024-07-11 15:15:19 +0900

    Fix: dto 리턴값 Boolean으로 수정

--- src/main/java/com/test/mytest/dto/MemberResponseDto.java
+++ src/main/java/com/test/mytest/dto/MemberResponseDto.java
@@ -1,13 +1,9 @@
 package com.test.mytest.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 
-
 public class MemberResponseDto {
-    @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
     @Getter
     @Builder
     public static class MemberInfoDto{
