73b83211a41189da75b0a6a3f1a9b5436169c365
mijeong <victon777@naver.com>
2024-07-12 12:33:38 +0900

    Feature: 배포 환경 설정

--- src/main/java/com/test/mytest/controller/TestController.java
+++ src/main/java/com/test/mytest/controller/TestController.java
@@ -1,10 +1,7 @@
 package com.test.mytest.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,12 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping()
-    public ResponseEntity<?> testMethod(@RequestParam(name="name")String name){
-        try {
-            return ResponseEntity.ok(name);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(e.getMessage());
-        }
+    public String test(){
+        return "Hello World";
     }
 }
